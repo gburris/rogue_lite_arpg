888f727670a0a18361b2c22694f0294beb82cf57
Kayton <kaytonfletcher@gmail.com>
2024-12-31 19:13:27 -0800

    Add time_to_live to projectiles, auto-despawning if no collisions occur

--- src/plugins/projectile_plugin.rs
+++ src/plugins/projectile_plugin.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 use crate::events::ProjectileHitEvent;
 use crate::systems::check_projectile_collision;
+use crate::systems::despawn::despawn_projectiles;
 use crate::systems::handle_projectile_hits;
 use crate::systems::move_projectiles;
 use crate::systems::process_burning;
@@ -12,9 +13,15 @@ impl Plugin for ProjectilePlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<ProjectileHitEvent>()
             .add_event::<ProjectileHitEvent>()
-            .add_systems(Update, move_projectiles)
-            .add_systems(Update, check_projectile_collision)
-            .add_systems(Update, handle_projectile_hits)
-            .add_systems(Update, process_burning);
+            .add_systems(
+                Update,
+                (
+                    move_projectiles,
+                    check_projectile_collision,
+                    handle_projectile_hits,
+                    process_burning,
+                    despawn_projectiles,
+                ),
+            );
     }
 }
---
+++ src/systems/despawn.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+
+use crate::components::Projectile;
+
+/**
+ * For each projectile in world space, we check if time to live has expired and then despawn it
+ */
+pub fn despawn_projectiles(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut Projectile)>,
+) {
+    for (entity, mut projectile) in query.iter_mut() {
+        projectile.time_to_live.tick(time.delta());
+        if projectile.time_to_live.finished() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}

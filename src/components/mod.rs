eb7656bb26d486854b10f63ee55b960b8cd58001
gburris <gburris.13@gmail.com>
2025-01-03 13:37:09 -0800

    finish refactoring levels and map, left some stuff in components to build out later

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,18 +1,9 @@
-pub mod animation_indices;
-pub mod burning_effect;
 pub mod damage_effect;
-pub mod freezing_effect;
 pub mod health;
 pub mod health_bar;
 pub mod speed;
-pub mod warp_zone;
 
-pub use animation_indices::*;
-pub use burning_effect::BurningEffect;
 pub use damage_effect::DamageEffect;
-pub use freezing_effect::*;
 pub use health::Health;
 pub use health_bar::HealthBar;
 pub use speed::Speed;
-pub use warp_zone::Level;
-pub use warp_zone::WarpZone;

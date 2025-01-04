ba523be3d1ca5aed8df4c7181c0fbe9ad4660611
KaytonFletcher <kayton@fastmail.com>
2025-01-04 15:57:53 -0800

    Make Portal Enum component with types WarpZone and StartingPortal, reducing duplicate portal logic

--- src/map/systems/despawn.rs
+++ src/map/systems/despawn.rs
@@ -2,22 +2,17 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::map::TilemapId;
 
 use crate::map::{
-    components::{StartingPortal, WarpZone},
+    components::Portal,
     events::{DespawnAllPortals, DespawnAllTiles},
 };
 
 pub fn despawn_all_portals(
     _: Trigger<DespawnAllPortals>,
     mut commands: Commands,
-    mut portal_query: Query<Entity, With<StartingPortal>>,
-    mut warpzone_query: Query<Entity, With<WarpZone>>,
+    mut portal_query: Query<Entity, With<Portal>>,
 ) {
     debug!("Depawning all portals");
     for entity in portal_query.iter_mut() {
-        commands.entity(entity).despawn_recursive();
-    }
-
-    for entity in warpzone_query.iter_mut() {
         commands.entity(entity).despawn_recursive();
     }
 }

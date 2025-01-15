15b09af17e5bab48d44e2f879cf1bffc222f6fef
Spencer0 <Spencer0@users.noreply.github.com>
2025-01-15 04:54:52 +0000

    deploy: 83665189aeeec7073b4c6cdb59e20056d5d0e8f5

--- src/ui/pause_menu.rs
+++ src/ui/pause_menu.rs
@@ -23,41 +23,143 @@ pub fn spawn_main_menu(mut commands: Commands) {
             Node {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
                 ..default()
             },
-            BackgroundColor::from(Color::BLACK.with_alpha(0.9)), // want to allow game to be seen in background
-            Visibility::Visible, // created pause menu should be hidden to start
-            // render this above in-game UI such as player health and score
             GlobalZIndex(1),
+            Visibility::Visible,
         ))
         .with_children(|parent| {
-            parent.spawn((
-                Text::new("Game Paused"),
-                TextFont {
-                    font_size: 70.0,
-                    ..default()
-                },
-                Node {
-                    top: Val::Percent(-20.0),
-                    ..default()
-                },
-            ));
-            parent.spawn((
-                Text::new("Equipment"),
-                EquipmentMenuButton,
-                Button,
-                TextFont {
-                    font_size: 50.0,
-                    ..default()
-                },
-                Node {
-                    right: Val::Percent(20.0),
-                    ..default()
-                },
-            ));
+            // Header Section
+            parent
+                .spawn((
+                    Node {
+                        width: Val::Percent(100.0),
+                        height: Val::Px(120.0),
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    BackgroundColor::from(Color::srgb(0.1, 0.1, 0.1)),
+                ))
+                .with_children(|header| {
+                    header.spawn((
+                        Text::new("GAME PAUSED"),
+                        TextFont {
+                            font_size: 80.0,
+                            ..default()
+                        },
+                        Node::default(),
+                    ));
+                });
+
+            // Body Section (transparent)
+            parent
+                .spawn((
+                    Node {
+                        width: Val::Percent(100.0),
+                        flex_grow: 1.0,
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        row_gap: Val::Px(20.0),
+                        padding: UiRect::vertical(Val::Px(40.0)),
+                        ..default()
+                    },
+                    BackgroundColor::from(Color::srgba(0.0, 0.0, 0.0, 0.7)),
+                ))
+                .with_children(|body| {
+                    // Equipment Button
+                    spawn_menu_button(body, "EQUIPMENT", true);
+                    // Additional menu buttons (placeholders)
+                    spawn_menu_button(body, "INVENTORY", true);
+                    spawn_menu_button(body, "SKILLS", true);
+                });
+
+            // Footer Section
+            parent
+                .spawn((
+                    Node {
+                        width: Val::Percent(100.0),
+                        height: Val::Px(80.0),
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::SpaceBetween,
+                        align_items: AlignItems::Center,
+                        padding: UiRect::horizontal(Val::Px(40.0)),
+                        ..default()
+                    },
+                    BackgroundColor::from(Color::srgb(0.1, 0.1, 0.1)),
+                ))
+                .with_children(|footer| {
+                    // Player Stats
+                    footer
+                        .spawn((Node {
+                            flex_direction: FlexDirection::Row,
+                            column_gap: Val::Px(20.0),
+                            ..default()
+                        },))
+                        .with_children(|stats| {
+                            stats.spawn((
+                                Text::new("Level: 1"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                Node::default(),
+                            ));
+                            stats.spawn((
+                                Text::new("Health: 100.0 / 100.0"),
+                                TextFont {
+                                    font_size: 24.0,
+                                    ..default()
+                                },
+                                Node::default(),
+                            ));
+                        });
+
+                    // Exit Instructions
+                    footer.spawn((
+                        Text::new("Press ESC to unpause"),
+                        TextFont {
+                            font_size: 24.0,
+                            ..default()
+                        },
+                        Node::default(),
+                    ));
+                });
         });
+}
+
+fn spawn_menu_button(parent: &mut ChildBuilder, text: &str, is_equipment: bool) {
+    let mut button = parent.spawn((
+        Button,
+        Node {
+            width: Val::Px(300.0),
+            height: Val::Px(60.0),
+            border: UiRect::all(Val::Px(2.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        BorderColor(Color::srgb(0.8, 0.8, 0.8)),
+        BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+    ));
+
+    if is_equipment {
+        button.insert(EquipmentMenuButton);
+    }
+
+    button.with_children(|button| {
+        button.spawn((
+            Text::new(text),
+            TextFont {
+                font_size: 32.0,
+                ..default()
+            },
+            Node::default(),
+        ));
+    });
 }
 
 pub fn spawn_pause_screen(mut commands: Commands) {
@@ -71,9 +173,8 @@ pub fn spawn_pause_screen(mut commands: Commands) {
             align_items: AlignItems::Center,
             ..default()
         },
-        BackgroundColor::from(Color::BLACK.with_alpha(0.9)), // want to allow game to be seen in background
-        Visibility::Visible, // created pause menu should be hidden to start
-        // render this above in-game UI such as player health and score
+        BackgroundColor::from(Color::BLACK.with_alpha(0.9)),
+        Visibility::Visible,
         GlobalZIndex(1),
     ));
 }

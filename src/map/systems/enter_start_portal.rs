1f10b074e1a594360df34671257131569dc09c02
Spencer Strickland <spencerstrickland1@hotmail.com>
2025-01-22 19:30:19 -0800

    Fix merge round 2

--- src/map/systems/enter_start_portal.rs
+++ src/map/systems/enter_start_portal.rs
@@ -14,7 +14,6 @@ pub fn enter_start_portal(
 ) {
     for _event in events.read() {
         commands.trigger(CleanupZone);
-        commands.trigger(CleanupCurrentWorldSpace);
         game_state.set(AppState::CreateInstance);
         playing_state.set(InGameState::Run);
     }

b1cd16e3a4c4b174886b4bc7b84377ad497deea8
gburris <gburris.13@gmail.com>
2025-01-23 19:17:53 -0800

    feat: implemented instance .ron to accept parameters for instance generation

--- src/map/systems/instance/mod.rs
+++ src/map/systems/instance/mod.rs
@@ -1,6 +1,7 @@
 pub mod finish_create_instance;
 pub mod generate_instance_layout;
 pub mod handle_instance_portal_enter;
+pub mod load_instance_data;
 pub mod render_instance_tilemap;
 pub mod spawn_instance_collisions_zones;
 pub mod spawn_instance_entities;
@@ -8,6 +9,7 @@ pub mod spawn_instance_entities;
 pub use finish_create_instance::finish_create_instance;
 pub use generate_instance_layout::generate_instance_layout;
 pub use handle_instance_portal_enter::handle_instance_portal_enter;
+pub use load_instance_data::*;
 pub use render_instance_tilemap::render_instance_tilemap;
 pub use spawn_instance_collisions_zones::spawn_instance_collisions_zones;
 pub use spawn_instance_entities::spawn_instance_entities;

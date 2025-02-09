9eff246103c3d34afd065ed5e32808daa1195e87
Spencer Strickland <spencerstrickland1@hotmail.com>
2025-02-09 13:16:44 -0800

    Desperately try to get WASM to actually work

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,38 @@ pub mod npc;
 pub mod player;
 pub mod progression;
 pub mod ui;
+
+use bevy::prelude::*;
+use wasm_bindgen::prelude::*;
+
+// This is the entry point for WASM
+#[wasm_bindgen(start)]
+pub fn start() {
+    // Enable better error messages for WASM
+    console_error_panic_hook::set_once();
+
+    // Start your Bevy app - this should be similar to your main.rs setup
+    App::new()
+        .add_plugins((SetupPlugin, AnimationPlugin, SchedulePlugin))
+        .add_plugins((AssetLoadingPlugin, TilemapPlugin)) // 3rd party crates
+        // Core plugins
+        .add_plugins((
+            DespawnPlugin,
+            MovementPlugin,
+            CombatPlugin,
+            ProgressionPlugin,
+        ))
+        // Entity-domain plugins (map, player, enemy, npc, etc..)
+        .add_plugins((
+            MapPlugin,
+            EquipmentPlugin,
+            GroundedPlugin,
+            PlayerPlugin,
+            EnemyPlugin,
+            NPCPlugin,
+            ChestPlugin,
+        ))
+        // UI plugins group
+        .add_plugins(UIPlugin)
+        .run();
+}

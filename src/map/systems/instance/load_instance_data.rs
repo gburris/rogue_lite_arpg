456fbad3cc8249bc5eb50310cb56bec7645e8877
Spencer Strickland <spencerstrickland1@hotmail.com>
2025-02-10 09:47:29 -0800

    Give alternative RON methods for WASM

--- src/map/systems/instance/load_instance_data.rs
+++ src/map/systems/instance/load_instance_data.rs
@@ -5,15 +5,29 @@ use bevy::{prelude::Commands, scene::ron::de::from_reader};
 use crate::map::{InstanceAssets, InstanceConfig, InstanceType};
 
 pub fn setup_instance_data(mut commands: Commands) {
-    let instance_config = load_instance_data("assets/config/instances.ron");
+    let instance_config = load_instance_data();
     commands.insert_resource(InstanceAssets { instance_config });
 }
 
-fn load_instance_data(path: &str) -> HashMap<String, InstanceType> {
-    let file = File::open(path).expect("Failed to open RON file");
+#[cfg(not(target_arch = "wasm32"))]
+fn load_instance_data() -> HashMap<String, InstanceType> {
+    let file = File::open("assets/config/instances.ron").expect("Failed to open RON file");
     let reader = BufReader::new(file);
 
     match from_reader::<_, InstanceConfig>(reader) {
+        Ok(data) => data.instances,
+        Err(e) => {
+            eprintln!("Failed to parse RON file: {:?}", e);
+            panic!("RON parsing error");
+        }
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn load_instance_data() -> HashMap<String, EnemyType> {
+    const INSTANCE_RON: &str = include_str!("../../assets/config/instances.ron");
+
+    match from_str::<EnemiesConfig>(INSTANCE_RON) {
         Ok(data) => data.instances,
         Err(e) => {
             eprintln!("Failed to parse RON file: {:?}", e);

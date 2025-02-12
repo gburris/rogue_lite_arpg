e5e6d639f9f443e431e77a4d9abf9319925ffe1c
KaytonFletcher <kayton@fastmail.com>
2025-02-11 21:12:50 -0800

    Fix item pickups and remove unused dependency

--- src/items/grounded/on_grounded_item_interaction.rs
+++ src/items/grounded/on_grounded_item_interaction.rs
@@ -11,29 +11,26 @@ pub fn on_grounded_item_input_interaction(
     _: Trigger<AttemptInteractionInput>,
     mut commands: Commands,
     colliding_items: Query<(Entity, &CollidingEntities), (With<Grounded>, Without<Autoloot>)>,
-    mut inventory_query: Query<&mut Inventory>,
-    player_query: Single<Entity, With<Player>>,
+    player: Single<(Entity, &mut Inventory), With<Player>>,
 ) {
-    let player_entity = player_query.into_inner();
+    let (player_entity, mut inventory) = player.into_inner();
 
     for (item_entity, colliding_entities) in colliding_items.iter() {
         if colliding_entities.contains(&player_entity) {
-            if let Ok(mut inventory) = inventory_query.get_single_mut() {
-                if inventory.add_item(item_entity).is_ok() {
-                    commands.entity(player_entity).add_child(item_entity);
+            if inventory.add_item(item_entity).is_ok() {
+                commands.entity(player_entity).add_child(item_entity);
 
-                    commands
-                        .entity(item_entity)
-                        .remove::<Grounded>()
-                        .remove::<Collider>()
-                        .remove::<Sensor>()
-                        .remove::<CollidingEntities>()
-                        .remove::<LiveDuration>()
-                        .remove::<CollisionLayers>()
-                        .insert(Visibility::Hidden);
-                } else {
-                    warn!("Inventory is full!")
-                }
+                commands
+                    .entity(item_entity)
+                    .remove::<Grounded>()
+                    .remove::<Collider>()
+                    .remove::<Sensor>()
+                    .remove::<CollidingEntities>()
+                    .remove::<LiveDuration>()
+                    .remove::<CollisionLayers>()
+                    .insert(Visibility::Hidden);
+            } else {
+                warn!("Inventory is full!")
             }
         }
     }

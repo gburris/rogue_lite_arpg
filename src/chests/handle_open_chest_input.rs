83a4b246dd0a6717533766d9dd684b3c9c808fd0
Spencer Strickland <spencerstrickland1@hotmail.com>
2025-02-11 18:15:29 -0800

    Query -> Single for multiple player access points

--- src/chests/handle_open_chest_input.rs
+++ src/chests/handle_open_chest_input.rs
@@ -10,9 +10,9 @@ pub fn handle_open_chest_input(
     _: Trigger<AttemptInteractionInput>,
     mut commands: Commands,
     query: Query<(&Parent, &CollidingEntities), With<ChestInteractionRadius>>,
-    player_query: Query<Entity, With<Player>>,
+    player_query: Single<Entity, With<Player>>,
 ) {
-    let player_entity = player_query.single();
+    let player_entity = player_query.into_inner();
     for (parent, colliding_entities) in &query {
         // Check if any of the colliding entities is the player
         if colliding_entities.contains(&player_entity) {
--- src/enemy/systems/handle_enemy_defeated.rs
+++ src/enemy/systems/handle_enemy_defeated.rs
@@ -21,7 +21,7 @@ pub fn on_enemy_defeated(
     trigger: Trigger<DefeatedEvent>,
     mut commands: Commands,
     mut defeated_enemy_query: Query<(&Experience, &Transform, Option<&Inventory>), With<Enemy>>,
-    mut player_query: Query<(&PlayerStats, &mut PlayerExperience), With<Player>>,
+    player_query: Single<(&PlayerStats, &mut PlayerExperience), With<Player>>,
     item_query: Query<&Item>,
 ) {
     let mut rng = thread_rng();
@@ -29,41 +29,40 @@ pub fn on_enemy_defeated(
     if let Ok((experience_to_gain, transform, inventory)) =
         defeated_enemy_query.get_mut(trigger.entity())
     {
-        if let Ok((player_stats, mut experience)) = player_query.get_single_mut() {
-            //Give EXP to the player
-            experience.current += experience_to_gain.base_exp;
+        let (player_stats, mut experience) = player_query.into_inner();
+        //Give EXP to the player
+        experience.current += experience_to_gain.base_exp;
 
+        //Drop their items based on drop rate
+        //Drop their gold based on players luck stat
+        if let Some(inventory) = inventory {
             //Drop their items based on drop rate
-            //Drop their gold based on players luck stat
-            if let Some(inventory) = inventory {
-                //Drop their items based on drop rate
-                for item_entity in inventory.items.iter() {
-                    if let Ok(item_result) = item_query.get(*item_entity) {
-                        let roll = rng.gen_range(0.0..1.0);
-                        if roll > (1.0 - item_result.drop_rate) {
-                            commands.trigger_targets(
-                                ItemToGroundEvent {
-                                    origin_position: transform.translation,
-                                },
-                                *item_entity,
-                            );
-                        }
+            for item_entity in inventory.items.iter() {
+                if let Ok(item_result) = item_query.get(*item_entity) {
+                    let roll = rng.gen_range(0.0..1.0);
+                    if roll > (1.0 - item_result.drop_rate) {
+                        commands.trigger_targets(
+                            ItemToGroundEvent {
+                                origin_position: transform.translation,
+                            },
+                            *item_entity,
+                        );
                     }
                 }
-                if rng.gen_range(0.0..1.0) < (0.1 + (player_stats.luck as f32 / 100.0)) {
-                    commands.trigger(GoldDropEvent {
-                        drop_location: *transform,
-                        amount: inventory.coins,
-                    });
-                }
             }
-
-            commands
-                .entity(trigger.entity())
-                .insert(LiveDuration::new(2.0))
-                .insert(ActionState::Defeated)
-                .insert(ColliderDisabled)
-                .insert(Invulnerable::death());
+            if rng.gen_range(0.0..1.0) < (0.1 + (player_stats.luck as f32 / 100.0)) {
+                commands.trigger(GoldDropEvent {
+                    drop_location: *transform,
+                    amount: inventory.coins,
+                });
+            }
         }
+
+        commands
+            .entity(trigger.entity())
+            .insert(LiveDuration::new(2.0))
+            .insert(ActionState::Defeated)
+            .insert(ColliderDisabled)
+            .insert(Invulnerable::death());
     }
 }

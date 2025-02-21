9837ee9f19e321a165d4eaf09c4bc36ff10dfd54
KaytonFletcher <kayton@fastmail.com>
2025-02-21 08:02:07 -0800

    Fix gold drops

--- src/econ/currency.rs
+++ src/econ/currency.rs
@@ -1,16 +1,25 @@
 use avian2d::prelude::*;
 use bevy::prelude::*;
 
-use crate::{items::inventory::Inventory, player::Player};
+use crate::{configuration::GameCollisionLayer, items::inventory::Inventory, player::Player};
 
 #[derive(Component)]
+#[require(
+    Collider(|| Collider::circle(10.0)),
+    CollisionLayers(|| CollisionLayers::new(
+        GameCollisionLayer::Interaction,
+        [GameCollisionLayer::Player]
+    )),
+    Sensor,
+    CollidingEntities,
+)]
 pub struct Currency {
     pub value: u32,
 }
 
 pub fn handle_currency_collisions(
     mut commands: Commands,
-    currency_query: Query<(Entity, &Currency, &CollidingEntities), With<Currency>>,
+    currency_query: Query<(Entity, &Currency, &CollidingEntities)>,
     player: Single<(Entity, &mut Inventory), With<Player>>,
 ) {
     let (player_entity, mut player_inventory) = player.into_inner();

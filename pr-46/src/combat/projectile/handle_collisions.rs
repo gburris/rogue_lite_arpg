76ff7f7b45409adcd2a9a45177a17155ab7bfdcd
Spencer0 <Spencer0@users.noreply.github.com>
2025-03-20 17:01:58 +0000

    deploy: 87736765d4332c75f366535ff317c07bbb3b4a34

---
+++ pr-46/src/combat/projectile/handle_collisions.rs
@@ -0,0 +1,34 @@
+use avian2d::prelude::*;
+use bevy::prelude::*;
+
+use crate::combat::{
+    damage::{AttemptDamageEvent, Damage}, projectile::components::*, shield::components::ProjectileReflection, Health
+};
+
+pub fn handle_projectile_collisions(
+    mut commands: Commands,
+    projectile_query: Query<(&Projectile, &CollidingEntities, Entity)>,
+    health_query: Query<&Health>,
+    reflector_query: Query<&ProjectileReflection>,
+) {
+    for (projectile, colliding_entities, projectile_entity) in projectile_query.iter() {
+        for &colliding_entity in colliding_entities.iter() {
+            // If the thing we collide with has health, lets try to damage it!
+            if health_query.contains(colliding_entity) {
+                commands.trigger_targets(
+                    AttemptDamageEvent {
+                        damage: Damage::Range(projectile.damage),
+                        damage_source: Some(projectile_entity),
+                    },
+                    colliding_entity,
+                );
+            }
+            if reflector_query.contains(colliding_entity) {
+                continue;
+            }
+            // despawn projectile and ignore further collisions after ANY collision
+            commands.entity(projectile_entity).despawn_recursive();
+            return;
+        }
+    }
+}
